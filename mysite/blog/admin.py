b4d271cfd5f073c3015ce2ef831f1aa46c5d0428
vitaly <onychkov@gmail.com>
2020-09-08 17:12:43 +0300

    continue 31/08/2020

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -11,7 +11,14 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+    # slug генерируется автоматически из поля title с помощью атрибута prepopulated_fields
 
 
- # slug генерируется автоматически из поля title с помощью атрибута prepopulated_fields
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
 
