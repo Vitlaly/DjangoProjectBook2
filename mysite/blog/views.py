e063562bad8880b75e8dbb8e224fe5a112b326e2
vitaly <onychkov@gmail.com>
2020-09-21 17:49:55 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mysite/blog/forms.py
    #	mysite/blog/templates/blog/post/detail.html
    #	mysite/blog/templates/blog/post/share.html
    #	mysite/blog/urls.py
    #	mysite/blog/views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -22,9 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-
 def post_share(request, post_id):
-
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
     if request.method == 'POST':
