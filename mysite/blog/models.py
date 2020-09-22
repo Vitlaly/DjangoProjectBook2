6d212fd54e9537a8caf69cf998c350327cd55546
vitaly <onychkov@gmail.com>
2020-09-22 17:45:58 +0300

    current state 22.09.2020-v.1.2

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
 
+
 class PublishedManager(models.Manager):
+
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
 
