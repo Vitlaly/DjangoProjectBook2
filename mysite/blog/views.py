14c7c3bc493d0722a5031719831c57d64ea18043
Vitaly <onychkov@gmail.com>
2020-12-13 21:08:30 +0200

    NEW Database PosgreSQL

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,8 +7,8 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
-from .forms import  EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 # class PostListView(ListView):
 #     queryset = Post.published.all()
@@ -84,6 +84,10 @@ def post_search(request):
         form = SearchForm(request.GET)
     if form.is_valid():
         query = form.cleaned_data['query']
-        results = Post.objects.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+        search_vector = SearchVector('title', 'body')
+        search_query = SearchQuery(query)
+        results = Post.objects.annotate(
+            search=search_vector,
+            rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
